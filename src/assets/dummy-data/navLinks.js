f55bc001ea7057ec675e81d8a0f739b813366f64
ASW1234 <52271086+ASW1234@users.noreply.github.com>
2023-05-04 21:11:30 +0530

    Carstow coding

---
+++ src/assets/dummy-data/navLinks.js
@@ -0,0 +1,20 @@
+const navLinks = [
+  {
+    path: "/dashboard",
+    icon: "ri-apps-2-line",
+    display: "Dashboard",
+  },
+  {
+    path: "/bookings",
+    icon: "ri-taxi-line",
+    display: "Booking",
+  },
+ 
+  {
+    path: "/settings",
+    icon: "ri-settings-2-line",
+    display: "Settings",
+  },
+];
+
+export default navLinks;
---
+++ src/components/UI/AboutSection.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "../../styles/about-section.css";
+import aboutImg from "../../assets/all-images/cars-img/tow.png";
+
+const AboutSection = ({ aboutClass }) => {
+  return (
+    <section
+      className="about__section"
+      style={
+        aboutClass === "aboutPage"
+          ? { marginTop: "0px" }
+          : { marginTop: "280px" }
+      }
+    >
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className="about__section-content">
+              <h4 className="section__subtitle">About Us</h4>
+              <h2 className="section__title">Welcome to CarsTow service</h2>
+              <p className="section__description">
+                Welcome to the world of car towing services! 
+                Whether you are a new driver or an experienced one, 
+                it's always important to know about car towing services 
+                and what they can offer you.We will introduce you to the 
+                world of car towing services and help you understand how                 
+                they can assist you when you need them the most.
+              </p>
+
+              <div className="about__section-item d-flex align-items-center">
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  amet.
+                </p>
+
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  amet.
+                </p>
+              </div>
+
+              <div className="about__section-item d-flex align-items-center">
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  amet.
+                </p>
+
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  amet.
+                </p>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg="6" md="6">
+            <div className="about__img">
+              <img src={aboutImg} alt="" className="w-100" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default AboutSection;
